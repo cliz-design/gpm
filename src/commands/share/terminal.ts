001a0560884a527dcc463ea5d197efc4db8102a0
whatwewant <tobewhatwewant@outlook.com>
2022-10-12 19:42:00 +0800

    feat: share terminal support server + client mode

--- src/commands/share/terminal.ts
+++ src/commands/share/terminal.ts
@@ -1,6 +1,16 @@
 import { defineSubCommand } from '@cliz/cli';
-import { startServer } from '@cliz/web-terminal/lib/core';
+import { startServer, startClient } from '@cliz/web-terminal/lib/core';
 
+/**
+ * example:
+ *    server:
+ *      using link: gpm share terminal
+ *      use code: gpm share terminal --disable-link
+ *    
+ *    client:
+ *      using link: open url in browser
+ *      use code: gpm share terminal --client -c <code>
+ */
 export default defineSubCommand((createCommand) => {
   return createCommand('Share terminal')
     .option('-c, --code <code>', 'Use custom code', {
@@ -9,13 +19,29 @@ export default defineSubCommand((createCommand) => {
     .option('-r, --relay <relay>', 'Use custom relay', {
       default: process.env.REPLAY || 'wss://web-terminal.zcorky.com/terminal',
     })
+    .option('--disable-link', 'Disable link', {
+      default: false,
+    })
+    .option('--client', 'Set as client, default: false', {
+      default: false,
+    })
     .option('--verbose', 'Show Command Log')
     .action(({ args, options }) => {
+      let useLink = true;
+      let isClient = !!options.client;
+      if (!!options.disableLink) {
+        useLink = false;
+      }
+
       const _options: any = {
         ...args,
         ...options,
-        useLink: true,
+        useLink,
       };
+
+      if (!!isClient) {
+        return startClient(_options);
+      }
 
       return startServer(_options);
     });
